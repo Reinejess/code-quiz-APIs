0c0165e6c98fb933467e8698e23ef8e99424a0c2
Reinejess <lore.jessmedina@gmail.com>
2022-04-17 11:06:54 -0500

    submit button

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var startGame = document.getElementById('quiz');
+var submitButton = document.getElementById('submit');
+var results = document.getElementById('results');
+
+
+function showResults() {
+
+}
+
+submitButton.addEventListener('click', showResults);
